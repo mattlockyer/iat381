3a82b8e1a2a23fb9d1557a989d98386e25c7ee56
Matt Lockyer <mattdlockyer@gmail.com>
2014-12-09 09:32:56 -0800

    added final touched on framework

--- components/main-screen/main-screen.js
+++ components/main-screen/main-screen.js
@@ -2,6 +2,10 @@
 
 "use-strict";
 
+var APP = {};
+
+
+
 Polymer("main-screen", {
   /*******************************
   * Events
@@ -11,68 +15,78 @@ Polymer("main-screen", {
   },
   ready:function() {
 
-    //setTimeout(function() {
+  //setTimeout(function() {
 
 
- //console.log('main screen ready');
- var scaffold = this.scaffold = this.$.scaffold;
- var menu = this.menu = this.$.menu;
- var pages = this.pages = this.$.pages;
+  //console.log('main screen ready');
+  var self = this;
+  var scaffold = this.scaffold = this.$.scaffold;
+  var menu = this.menu = this.$.menu;
+  var pages = this.pages = this.$.pages;
+  //build sections array
+  var children = this.children;
+  var sections = this.sections = [];
 
-    //build sections array
-    var sections = this.children;
-    this.sections = [];
-    for (var i = 0; i < sections.length; i++) {
-      //build section object for template
-      var section = {};
-      var attributes = sections[i].attributes;
-      for (var j = 0; j < attributes.length; j++) {
-        var attr = attributes[j];
-        section[attr.name] = attr.value;
-      }
-      this.sections.push(section);
-      //add a page for this section to core-pages template
-      var page = document.createElement('section');
-      var wrap = document.createElement('div');
-      wrap.innerHTML = sections[i].innerHTML;
-
-      var buttonWrap = document.createElement('div');
-
-      if (i > 0) {
-         //back button
-         var button = document.createElement('core-icon-button');
-         button.setAttribute('icon', 'arrow-back');
-         button.onclick = function() {
-          pages.selected = pages.selected - 1;
-          menu.children[pages.selected + 2].classList.remove('core-selected');
-          menu.children[pages.selected + 1].classList.add('core-selected');
-        };
-        buttonWrap.appendChild(button);
-      }
-      if (i < sections.length - 1) {
-         //forward button
-         var button = document.createElement('core-icon-button');
-         button.setAttribute('icon', 'arrow-forward');
-         button.onclick = function() {
-          pages.selected = pages.selected + 1;
-          menu.children[pages.selected].classList.remove('core-selected');
-          menu.children[pages.selected + 1].classList.add('core-selected');
-        };
-        button.style.float = 'right';
-        buttonWrap.appendChild(button);
-      }
-
-      wrap.appendChild(buttonWrap);
-      page.appendChild(wrap);
-      this.pages.appendChild(page);
+  for (var i = 0; i < children.length; i++) {
+    //build section object for template
+    var section = {};
+    var attributes = children[i].attributes;
+    for (var j = 0; j < attributes.length; j++) {
+      var attr = attributes[j];
+      section[attr.name] = attr.value;
     }
-    //activate first menu item
-    var menu = this.menu;
-    setTimeout(function() {
-      menu.children[1].classList.add('core-selected');
-    }, 250);
-    //show
-    this.classList.remove('hidden');
+    sections.push(section);
+    //add a page for this section to core-pages template
+    var page = document.createElement('section');
+    var wrap = document.createElement('div');
+    wrap.innerHTML = children[i].innerHTML;
+
+    var buttonWrap = document.createElement('div');
+
+    if (i > 0) {
+       //back button
+       var button = document.createElement('core-icon-button');
+       button.setAttribute('icon', 'arrow-back');
+       button.onclick = function() {
+        self.changePage(sections[pages.selected - 1].name);
+      };
+      buttonWrap.appendChild(button);
+    }
+    if (i < children.length - 1) {
+       //forward button
+       var button = document.createElement('core-icon-button');
+       button.setAttribute('icon', 'arrow-forward');
+       button.onclick = function() {
+        self.changePage(sections[pages.selected + 1].name);
+      };
+      button.style.float = 'right';
+      buttonWrap.appendChild(button);
+    }
+
+    wrap.appendChild(buttonWrap);
+    page.appendChild(wrap);
+    this.pages.appendChild(page);
+  }
+  //switch to the right page
+  var setFirstPage = function() {
+    if (!menu.children[1]) {
+      setTimeout(setFirstPage, 25);
+      return;
+    }
+    console.log(window.location.hash);
+    if (window.location.hash) {
+      self.changePage(window.location.hash.substring(1));
+    } else {
+      self.changePage(sections[0].name, false, true);
+    }
+  };
+  setFirstPage();
+  //attach window history event listener
+  window.onpopstate = function(event) {
+    self.changePage(window.location.hash.substring(1), true);
+  };
+  //show
+  this.classList.remove('hidden');
 
 
   //}, 2000);
@@ -84,15 +98,34 @@ attached:function() {
   /*******************************
   * Methods
   *******************************/
+  currentPage:null,
+  changePage:function(name, back, replace) {
+    //console.log('currentPage: ' + this.currentPage);
+    //console.log('name: ' + name);
+    //console.log('');
+    if (name === this.currentPage) {
+      return;
+    }
+    this.currentPage = name;
+    for (var i = 0; i < this.sections.length; i++) {
+      this.menu.children[i+1].classList.remove('core-selected');
+      if (this.sections[i].name === name) {
+        this.pages.selected = i;
+        this.menu.children[i+1].classList.add('core-selected');
+      }
+    }
+    console.log('history pushed: ' + name);
+    if (back) return;
+    if (!replace) {
+      history.pushState({}, 'iat381 - ' + name, window.location.href.split('#')[0] + '#' + name);
+    } else {
+      history.replaceState({}, 'iat381 - ' + name, window.location.href.split('#')[0] + '#' + name);
+    }
+  },
   onMenuClose:function() {
     this.scaffold.closeDrawer();
   },
   onMenuClick:function(e, detail) {
-    for (var i = 1; i < this.sections.length + 1; i++)
-      this.menu.children[i].classList.remove('core-selected');
-    this.pages.selected = parseInt(detail.item.attributes.num.value);
-    detail.item.classList.add('core-selected');
-    //untested don't know how to rig properly
-    //history.pushState({foo: "bar"}, "page 2", window.location.href.split('#')[0] + '#' + this.sections[this.pages.selected].name);
+    this.changePage(this.sections[parseInt(detail.item.attributes.num.value)].name);
   }
 });
