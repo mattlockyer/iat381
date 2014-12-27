5d28a2b0139fbfb3123642ab64ae7d59a9148e69
Matt Lockyer <mattdlockyer@gmail.com>
2014-12-27 10:35:03 -0800

    fixed mobile scrolling issue, added calendar

--- components/main-screen/main-screen.js
+++ components/main-screen/main-screen.js
@@ -8,16 +8,19 @@ var APP = {};
 
 Polymer("main-screen", {
   /*******************************
+  * Properties
+  *******************************/
+  isMobile:false,
+  currentPage:null,
+  /*******************************
   * Events
   *******************************/
   created:function() {
     //console.log('main screen created');
+    this.isMobile = (function(a,b){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4)))return true;else return false})(navigator.userAgent||navigator.vendor||window.opera);
   },
+
   ready:function() {
-
-  //setTimeout(function() {
-
-
   //console.log('main screen ready');
   var self = this;
   var scaffold = this.scaffold = this.$.scaffold;
@@ -42,11 +45,12 @@ Polymer("main-screen", {
     wrap.innerHTML = children[i].innerHTML;
 
     var buttonWrap = document.createElement('div');
+    buttonWrap.classList.add('button-wrap');
 
     if (i > 0) {
        //back button
-       var button = document.createElement('core-icon-button');
-       button.setAttribute('icon', 'arrow-back');
+       var button = this.$.menuPrev.cloneNode(true);
+       button.classList.remove('hidden');
        button.onclick = function() {
         self.changePage(sections[pages.selected - 1].name);
       };
@@ -54,15 +58,13 @@ Polymer("main-screen", {
     }
     if (i < children.length - 1) {
        //forward button
-       var button = document.createElement('core-icon-button');
-       button.setAttribute('icon', 'arrow-forward');
+       var button = this.$.menuNext.cloneNode(true);
+       button.classList.remove('hidden');
        button.onclick = function() {
         self.changePage(sections[pages.selected + 1].name);
       };
-      button.style.float = 'right';
       buttonWrap.appendChild(button);
     }
-
     wrap.appendChild(buttonWrap);
     page.appendChild(wrap);
     this.pages.appendChild(page);
@@ -73,7 +75,6 @@ Polymer("main-screen", {
       setTimeout(setFirstPage, 25);
       return;
     }
-    console.log(window.location.hash);
     if (window.location.hash) {
       self.changePage(window.location.hash.substring(1));
     } else {
@@ -88,21 +89,16 @@ Polymer("main-screen", {
   //show
   this.classList.remove('hidden');
 
-
-  //}, 2000);
-
 },
+
 attached:function() {
     //console.log('main screen attached');
   },
+
   /*******************************
   * Methods
   *******************************/
-  currentPage:null,
   changePage:function(name, back, replace) {
-    //console.log('currentPage: ' + this.currentPage);
-    //console.log('name: ' + name);
-    //console.log('');
     if (name === this.currentPage) {
       return;
     }
@@ -112,20 +108,31 @@ attached:function() {
       if (this.sections[i].name === name) {
         this.pages.selected = i;
         this.menu.children[i+1].classList.add('core-selected');
+        if (this.isMobile) {
+          console.log('mobile');
+          var page = this.pages.children[i];
+          page.style.height = this.scaffold.scroller.getBoundingClientRect().height + 'px';
+          page.style.overflow = 'scroll';
+          page.style.overflowX = 'hidden';
+        }
       }
     }
-    console.log('history pushed: ' + name);
     if (back) return;
-    if (!replace) {
-      history.pushState({}, 'iat381 - ' + name, window.location.href.split('#')[0] + '#' + name);
-    } else {
-      history.replaceState({}, 'iat381 - ' + name, window.location.href.split('#')[0] + '#' + name);
-    }
+    if (!replace) history.pushState({}, 'iat381 - ' + name, window.location.href.split('#')[0] + '#' + name);
+    else history.replaceState({}, 'iat381 - ' + name, window.location.href.split('#')[0] + '#' + name);
   },
+
   onMenuClose:function() {
     this.scaffold.closeDrawer();
   },
+
+  onMenuOpen:function() {
+    this.scaffold.openDrawer();
+  },
+
   onMenuClick:function(e, detail) {
     this.changePage(this.sections[parseInt(detail.item.attributes.num.value)].name);
   }
+
+  //end of component
 });
